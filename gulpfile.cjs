e51aedb3bef3938c273515d96137efa11735c3c1
Sébastien COGEZ <38463200+seb-cogez@users.noreply.github.com>
2024-05-12 10:36:39 +0200

    Amélioration gulpfile pour ne toucher que les pages modifiées

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -56,6 +56,18 @@ async function build() {
   })
 }
 
+async function minifyCss() {
+  builds.map(module => {
+    let listOfCss = ['src/css/'+module+'.css']
+    if(module === 'mathsmentales')
+      listOfCss = ['src/css/sprites.css', 'src/css/knacss.css','src/css/bulma-steps.css','src/css/mathsmentales.css']
+    return gulp.src(listOfCss)
+    .pipe(cleanCSS())
+    .pipe(concat(module+'.css'))
+    .pipe(gulp.dest('./public/css/'))
+  })
+}
+
 async function updateVersion() {
   builds.map(module => {
     let htmlPageName = module + '.html'
@@ -74,24 +86,58 @@ async function updateVersion() {
     fs.writeFileSync(destPath, updatedContent, 'utf8');
   })
 }
-
-gulp.task('minify-css-mm', () => {
-  return gulp.src(['src/css/sprites.css', 'src/css/knacss.css','src/css/bulma-steps.css','src/css/mathsmentales.css'])
-  .pipe(cleanCSS())
-  .pipe(concat('mathsmentales.min.css'))
-  .pipe(gulp.dest('./public/css/'))
-})
-gulp.task('minify-css-puzzle', () => {
-  return gulp.src(['src/css/puzzle.css'])
-  .pipe(cleanCSS())
-  .pipe(concat('puzzle.css'))
-  .pipe(gulp.dest('./public/css/'))
-})
-
 gulp.task('bump-major', () => bumpVersion({ major: true }));
 gulp.task('bump-minor', () => bumpVersion({ minor: true }));
 gulp.task('bump-patch', () => bumpVersion({ patch: true }));
 
+// création des modules de minification de js et css indépendants
+builds.forEach(module => {
+  gulp.task('minify-js-'+module, () => {
+    return rollupStream({
+      input: 'src/js/lib.' + module + '.js',
+      cache: cache,
+      output: { format: 'iife'}
+    }).on('error', err => {
+      console.log('Error : '+ err.message)
+    }).on('bundle', bundle => {
+      cache = bundle
+    }).pipe( source('lib.' + module + '.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/'))
+  })
+  gulp.task('minify-css-'+module, () => {
+    let listOfCss = ['src/css/'+module+'.css']
+    if(module === 'mathsmentales')
+      listOfCss = ['src/css/sprites.css', 'src/css/knacss.css','src/css/bulma-steps.css','src/css/mathsmentales.css']
+    return gulp.src(listOfCss)
+    .pipe(cleanCSS())
+    .pipe(concat(module+'.css'))
+    .pipe(gulp.dest('./public/css/'))
+  })
+  gulp.task('update-version-'+module, async () => {
+    let htmlPageName = module + '.html'
+    if (module === 'mathsmentales') htmlPageName = 'index.html'
+    const htmlPath = path.join(__dirname, './src/' + htmlPageName);
+    const destPath = path.join(__dirname, './public/' + htmlPageName)
+    const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+    // js
+    const regex = new RegExp('lib\\.' +module+ '\\.js\\?v=[\\d\\.]+')
+    let updatedContent = htmlContent.replace(regex, `lib.${module}.js?v=${packageJson.version}`);
+    // css - à mettre à jour pour autres modules
+    if(htmlPageName === 'index.html') {
+      updatedContent = updatedContent.replace('<link rel="stylesheet" href="css/sprites.css"><link rel="stylesheet" href="css/knacss.css" type="text/css" media="screen" /><link rel="stylesheet" href="css/bulma-steps.css" type="text/css" media="screen" /><link rel="stylesheet" href="css/mathsmentales.css" type="text/css" media="screen" />', '<link rel="stylesheet" href="css/mathsmentales.css?v='+packageJson.version+'" />')
+    } else {
+      const regex2 = new RegExp(module+'\\.css\\?v=[\\d\\.]+')
+      updatedContent = updatedContent.replace(regex2, module+'.css?v='+packageJson.version);
+    }
+    console.log(htmlPageName + ' mis à jour')
+    fs.writeFileSync(destPath, updatedContent, 'utf8');
+  })
+  gulp.task('maj-'+module, gulp.series('minify-js-'+module, 'minify-css-'+module, 'bump-patch', 'update-version-'+module))
+})
+
+gulp.task('minify-css', minifyCss)
 gulp.task('build', build);
 gulp.task('update-version', updateVersion);
-gulp.task('default', gulp.series('build', 'minify-css-mm', 'bump-patch', 'update-version'));
+gulp.task('default', gulp.series('build', 'minify-css', 'bump-patch', 'update-version'));
